aea655922a446e1b0918c99b54fda9213acff3bc
Vicente <carvalho.vicente0207@gmail.com>
2025-02-23 15:48:23 -0300

    Exercício 10 feito

---
+++ 10.js
@@ -0,0 +1,9 @@
+//Crie um objeto com suas informações (nome, idade, etc.) e exiba uma propriedade.
+var nome = "Vi"
+var idade = 16
+var gen = "Masc"
+
+console.log(`Seu nome é ${nome}`)
+console.log(`Sua idade é ${idade}`)
+console.log(`Seu Gênero é ${gen}`)
+// Arrumar depois, talvez?
