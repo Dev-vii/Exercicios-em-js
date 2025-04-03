ee07fe3dd3476da73034a2f0caaad004a6be059b
Vicente <carvalho.vicente0207@gmail.com>
2025-04-03 14:22:07 -0300

    Exercício 19 feito

---
+++ 19.js
@@ -0,0 +1,7 @@
+//Converta um número de segundos para o formato hh:mm:ss
+let seg = 3600
+let min = seg / 60
+let hrs = seg / 3600
+console.log(`${seg} segundos 
+    segundos em minutos: ${min} 
+    segundos em horas: ${hrs}`)
